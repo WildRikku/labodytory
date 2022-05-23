2f20e3b88470c881fb69b99c416e381879c1b2fc
JojoSim0n <josim@hrz.tu-chemnitz.de>
2022-05-23 13:11:32 +0200

    FixUnity project
    - moved files into Unity folder and fixed main unity project

---
+++ DeathIsOnlyTheBeginning/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void openOptions()
+    {
+
+    }
+
+    public void openCredits()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
