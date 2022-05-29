a21e1943323b402f00f6b18b80b948be8632a624
nightPrayer90 <m_steguweit@mail.de>
2022-05-29 14:05:07 +0200

    Tür Fix

---
+++ DeathIsOnlyTheBeginning/Assets/Scripts/RightLeg.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightLeg : MonoBehaviour
+{
+    public Player player;
+    public GameObject leg;
+    bool canGrab = false;
+
+    public GameObject useTextPrefab;
+    private GameObject objuseText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.E) && canGrab)
+        {
+            GrabLeg();
+            canGrab = false;
+
+            Destroy(this.gameObject);
+            DestroyUseText();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ShowUseText();
+            canGrab = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        DestroyUseText();
+        canGrab = false;
+    }
+
+    void ShowUseText()
+    {
+        if (useTextPrefab != null && objuseText == null)
+        {
+            objuseText = GameObject.Instantiate(useTextPrefab, transform.position + new Vector3(0.0f, -0.5f, 0), Quaternion.Euler(40f, 270f, 0f));
+            objuseText.GetComponent<TextMesh>().text = "<E> to grab";
+        }
+    }
+    void DestroyUseText()
+    {
+        if (objuseText)
+        {
+            Destroy(objuseText);
+            objuseText = null;
+        }
+    }
+
+
+    public void GrabLeg()
+    {
+        player.AddToAttachments(this.tag, leg);
+        if (tag == "RightLeg")
+            player.SpawnRightLeg();
+        
+    }
+}
